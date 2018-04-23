837f28c8a959ec9a8fd0756ac7bcce6ef4a364a9
Philipp Oppel <philipp.oppel@outlook.com>
2018-04-23 15:38:16 +0200

    Initial Commit

---
+++ mse/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  mse
+//
+//  Created by Philipp Oppel on 23.04.18.
+//  Copyright © 2018 Philipp Oppel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
